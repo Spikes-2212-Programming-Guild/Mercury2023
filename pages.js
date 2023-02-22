4d35066bec940474651b9ddcc547694a9937bd55
Microsh1t <122691746+Microsh1t@users.noreply.github.com>
2023-02-22 17:57:53 +0200

    somthing

---
+++ pages.js
@@ -0,0 +1,5 @@
+const pages = ['goodForm.html?qs=q0q4', 'grid.html?qs=q4','goodForm.html?qs=q9q10', 'grid.html?qs=q10','Timer.html?qs=q15', 'goodForm.html?qs=q16q21']
+
+function getPages(){
+    return pages;
+}
---
+++ timer.js
@@ -0,0 +1,47 @@
+let count = 0.0;
+
+let timer = document.createElement("input")
+timer.type = "text"
+timer.style.color = "white"
+timer.id = "timerText"
+document.body.appendChild(timer)
+timer.style.backgroundColor = "black"
+timer.value = "0.0";
+timer.style.fontSize = "32px"
+timer.style.width = "50%"
+timer.style.borderColor = "black"
+timer.style.textAlign = "center"
+
+let button1 = document.createElement("button")
+button1.onclick = timerClick
+button1.textContent = "start"
+button1.style.width = "95%"
+button1.style.backgroundColor = "white"
+button1.style.fontSize = "32px"
+document.body.appendChild(button1)
+
+
+
+function timerClick(){
+    if(button1.textContent === "start"){
+        button1.textContent = "stop"
+        count = Number(timer.value)
+    }
+    else{
+        button1.textContent = "start"
+    }
+}
+
+setInterval(function() {
+    if(button1.textContent == "stop"){
+        count += 0.1;
+        count = Math.round(count*10)
+        count /=10
+        if(count%1 == 0){
+            timer.value = count + ".0"
+        }
+        else{
+            timer.value = count;
+        }
+    }
+}, 100);
