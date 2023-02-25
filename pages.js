de86a79581f4204f1d9c74d6511b5e5cd5754206
Microsh1t <122691746+Microsh1t@users.noreply.github.com>
2023-02-25 10:56:46 +0000

    UwU | -_- | ^_^ | o_o | <^*-*^> | ( - ; | ) - :

--- pages.js
+++ pages.js
@@ -1,4 +1,4 @@
-const pages = ['goodForm.html?qs=q0q4', 'grid.html?qs=q4','goodForm.html?qs=q9q10', 'grid.html?qs=q10','Timer.html?qs=q15', 'goodForm.html?qs=q16q21']
+const pages = ['goodForm.html?qs=q0q4&title=מידע לפני המשחק', 'grid.html?qs=q4&title=אוטונומי - גריד','goodForm.html?qs=q9q10&title=אוטונומי - תחנת טעינה', 'grid.html?qs=q10&title=טלאופ - גריד','secondForm.html?qs=q15q17&title=טלאופ - תחנת טעינה', /*'goodForm.html?qs=q16q17&title=טלאופ - תחנת טעינה',*/ 'goodForm.html?qs=q17q21&title=מידע אחרי המשחק']
 
 function getPages(){
     return pages;
