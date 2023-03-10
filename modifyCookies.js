8ac08840b063a15fa5819e64dcf2d8885f82a062
Microsh1t <122691746+Microsh1t@users.noreply.github.com>
2023-03-10 12:40:24 +0000

    apple prodacts are almost as good as trash!

--- modifyCookies.js
+++ modifyCookies.js
@@ -1,13 +1,12 @@
 function createCookie(theTag, theContent){
-    document.cookie = theTag + "=" + theContent + "expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=Lax"
+    document.cookie = theTag + "=" + encodeURIComponent(theContent) + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=Lax"
 }
 
 function getCookieContent(theTag){
-    return document.cookie.split(theTag+"=")[1].split("expires")[0]
+    return decodeURIComponent(document.cookie.split(theTag+"=")[1].split(";")[0])
 }
 
 function eraseAllCookies(){
-
 // get all cookies
 const cookies = document.cookie.split(';');
 
@@ -20,5 +19,4 @@ const cookies = document.cookie.split(';');
         // set cookie expiration time to a date in the past
         document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     }
-
 }
